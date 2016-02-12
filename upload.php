1e79a23136febc0725587bc51466ad16c44d1802
josh <j9963232q@gmail.com>
2016-02-12 13:33:30 +0800

    S

--- upload.php
+++ upload.php
@@ -1,7 +1,16 @@
 <?php
-if($_FILES['file']['error']>0){
-  exit("檔案上傳失敗！");//如果出現錯誤則停止程式
+if ($_FILES["file"]["error"] > 0){
+　echo "Error: " . $_FILES["file"]["error"];
+}else{
+　echo "檔案名稱: " . $_FILES["file"]["name"]."<br/>";
+　echo "檔案類型: " . $_FILES["file"]["type"]."<br/>";
+　echo "檔案大小: " . ($_FILES["file"]["size"] / 1024)." Kb<br />";
+　echo "暫存名稱: " . $_FILES["file"]["tmp_name"];
+　
+　if (file_exists("upload/" . $_FILES["file"]["name"])){
+　　echo "檔案已經存在，請勿重覆上傳相同檔案";
+　}else{
+　　move_uploaded_file($_FILES["file"]["tmp_name"],"upload/".$_FILES["file"]["name"]);
+　}
 }
-move_uploaded_file($_FILES['file']['tmp_name'],'file/'.$_FILES['file']['name']);//複製檔案
-echo '<a href="file/'.$_FILES['file']['name'].'">file/'.$_FILES['file']['name'].'</a>';//顯示檔案路徑
 ?>
